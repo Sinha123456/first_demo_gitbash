b533d0c2831fdcd2c3c69825ff8e35fa12268dc4
user.email neetusinha07@gmail.comgit --listgit config --global user.email neetucleargitgit config --global user.email <neetusinha07@gmail.com>
2020-07-02 16:50:35 -0400

    function square, return addition of two square numbers

--- addition.py
+++ addition.py
@@ -1,8 +1,10 @@
 num1 = int(input("enter num1 "))
 num2 = int(input("enter num2 "))
-
-
 def add(num1, num2):
     addition = num1 + num2
     return addition
+def square(num1, num2):
+    sum_square = num1*num1+num2*num2
+    return sum_square
 print(add(num1, num2))
+print(square(num1,num2))
