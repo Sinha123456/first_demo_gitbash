a836ebf959d547b07c65dc0f3da37ae351dcc2cd
user.email neetusinha07@gmail.comgit --listgit config --global user.email neetucleargitgit config --global user.email <neetusinha07@gmail.com>
2020-07-02 16:37:27 -0400

    Revert "if num1==num2 don't print addition"
    This reverts commit 5eba5421ec9e6c520140b08efa7462a2dc3af4fd.

--- addition.py
+++ addition.py
@@ -1,12 +1,8 @@
 num1 = int(input("enter num1 "))
 num2 = int(input("enter num2 "))
 
+
 def add(num1, num2):
     addition = num1 + num2
-    if num1 == num2:
-        print("do not print eqaul numbers")
-    else:
-        return addition
-
-
+    return addition
 print(add(num1, num2))
